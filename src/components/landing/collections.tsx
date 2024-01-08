63443cc1f3effedec0e976a94e94f387efc55feb
Mateo Daza <mateodaza@gmail.com>
2024-01-08 13:19:12 -0500

    collections section

--- src/components/landing/collections.tsx
+++ src/components/landing/collections.tsx
@@ -1,47 +1,89 @@
 import React from 'react';
-import { Card, Image, Text, Title, Container, Flex } from '@mantine/core';
+import { Card, Image, Text, Title, Container, Flex, em } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 
 const cards = [
   {
-    title: 'GifmeBeer Membershio',
+    title: 'GifmeBeer Membership',
     type: 'membership',
     address: '0x0000...0000',
     image: '/images/collections/membership_card.png',
+    icon: '/images/icons/star_icon.png',
   },
   {
     title: 'Breweries Collection',
     type: 'brewery',
     address: '0x0000...0000',
     image: '/images/collections/brewery_card.png',
+    icon: '/images/icons/smile_icon.png',
   },
   {
     title: 'Festivals Collection',
     type: 'festival',
     address: '0x0000...0000',
     image: '/images/collections/festival_card.png',
+    icon: '/images/icons/smile_icon2.png',
   },
 ];
 
 const Collections = () => {
+  const isMobile = useMediaQuery(`(max-width: ${em(850)})`);
   return (
-    <Container my={100}>
-      <Flex align={'center'} justify='center' direction={'column'}>
-        <Title order={1} style={{ marginBottom: '2rem' }}>
-          Collections
-        </Title>
-        <Flex wrap={'wrap'} gap={'sm'}>
-          {cards.map((card) => (
-            <Card shadow='xs' style={{ width: '300px' }}>
-              <Image src={card.image} alt='Image 1' height={200} />
-              <Text style={{ marginTop: '1rem' }}>
-                <span style={{ marginRight: '0.5rem' }}>icon</span>
-                {card.title}
-              </Text>
-            </Card>
-          ))}
+    <Flex
+      w={'100%'}
+      py={100}
+      bg={'rgba(53, 210, 193, 1)'}
+      align={'center'}
+      justify='center'
+      direction={'column'}
+    >
+      <Container fluid>
+        <Flex w={'100%'} align={'center'} justify='center' direction={'column'}>
+          <Title
+            style={{
+              textTransform: 'uppercase',
+              fontSize: isMobile ? '4rem' : '6rem',
+              fontFamily: 'var(--font-metamorbit-latin)',
+            }}
+            order={1}
+          >
+            NFTs Collections
+          </Title>
+
+          <Flex direction={{ base: 'column', lg: 'row' }} gap={'md'} mt={40}>
+            {cards.map((card, index) => (
+              <Card key={index} w={400} h={'fit-content'} bg={'transparent'}>
+                <Flex direction='column' align={'center'} gap='md'>
+                  <Image src={card.image} alt='img' height={200} />
+                  <Flex
+                    mt={14}
+                    w={'200px'}
+                    align={'center'}
+                    justify={'center'}
+                    dir='row'
+                    gap={12}
+                  >
+                    <img
+                      src={card.icon}
+                      alt='icon'
+                      width={'16px'}
+                      height={'16px'}
+                    />
+                    <Text
+                      style={{
+                        fontFamily: 'var(--font-metamorbit-latin)',
+                      }}
+                    >
+                      {card.title}
+                    </Text>
+                  </Flex>
+                </Flex>
+              </Card>
+            ))}
+          </Flex>
         </Flex>
-      </Flex>
-    </Container>
+      </Container>
+    </Flex>
   );
 };
 
--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -4,12 +4,21 @@ import { Image, Flex, BackgroundImage } from '@mantine/core';
 const HeroPage = () => {
   return (
     <BackgroundImage src='/images/led_background.svg'>
-      <Flex w={1366} h={'100vh'} justify={'center'} align={'center'}>
+      <Flex h={'100vh'} justify={'center'} align={'center'}>
         <Flex h='80%' w='30%' justify={'flex-start'} align={'start'}>
-          <Image src='/images/gmb_logo.svg' alt='Logo' />
+          <Image w={'400px'} src='/images/gmb_logo.svg' alt='Logo' />
         </Flex>
-        <Flex w={'50%'} h={'80%'} justify={'flex-end'} align={'end'}>
-          <Image src='/images/landing_beers.png' alt='Beers' h='600px' />
+        <Flex
+          w={{ base: '100%', md: '50%' }}
+          h={'80%'}
+          justify={'flex-end'}
+          align={'end'}
+        >
+          <Image
+            src='/images/landing_beers.png'
+            alt='Beers'
+            h={{ base: '300px', sm: '400px', lg: '650px' }}
+          />
         </Flex>
       </Flex>
     </BackgroundImage>
