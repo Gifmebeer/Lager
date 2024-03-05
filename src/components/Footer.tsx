edf7197d9b281dfef6ec42427256a0eba08fd5eb
lauracapella <laura@laguapacomunicacion.com>
2024-03-05 12:36:05 +0100

    lista footer + tipografia after footer + iconos social media svg

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,128 @@
-import { Text, Container, ActionIcon, Group, rem, Flex } from '@mantine/core';
-import {
-  IconBrandXFilled,
-  IconBrandDiscordFilled,
-  IconBrandInstagram,
-} from '@tabler/icons-react';
+import { Text, Container, Group, rem, Flex, } from "@mantine/core";
+
+const TwitterIcon = () => (
+  <svg
+    width="35"
+    height="35"
+    viewBox="0 0 35 35"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_32_172)">
+      <path
+        d="M17.5 35C7.9 35 0 27.1 0 17.5C0 7.9 7.9 0 17.5 0C27.1 0 35 7.9 35 17.5C35 27.1 27.1 35 17.5 35ZM17.5 1C8.4 1 1 8.4 1 17.5C1 26.6 8.4 34 17.5 34C26.6 34 34 26.6 34 17.5C34 8.4 26.6 1 17.5 1Z"
+        fill="white"
+      />
+      <path
+        d="M17.1 18.5L16.5 17.6L11.5 10.4H13.7L17.8 16.2L18.4 17.1L23.7 24.7H21.5L17.1 18.5ZM19 16.2L25 9.30005H23.6L18.4 15.3L14.3 9.30005H9.5L15.7 18.4L9.5 25.7001H10.9L16.4 19.4L20.8 25.7001H25.6L19 16.2Z"
+        fill="white"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_32_172">
+        <rect width="35" height="35" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+const DiscordIcon = () => (
+  <svg
+    width="35"
+    height="35"
+    viewBox="0 0 35 35"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_32_180)">
+      <path
+        d="M17.5 35C7.9 35 0 27.1 0 17.5C0 7.9 7.9 0 17.5 0C27.1 0 35 7.9 35 17.5C35 27.1 27.1 35 17.5 35ZM17.5 1C8.4 1 1 8.4 1 17.5C1 26.6 8.4 34 17.5 34C26.6 34 34 26.6 34 17.5C34 8.4 26.6 1 17.5 1Z"
+        fill="white"
+      />
+      <path
+        d="M20.8 19.5001C19.8 19.5001 19 18.6001 19 17.5001C19 16.4001 19.8 15.5001 20.8 15.5001C21.8 15.5001 22.6 16.4001 22.6 17.5001C22.6 18.6001 21.8 19.5001 20.8 19.5001ZM14.2 19.5001C13.2 19.5001 12.4 18.6001 12.4 17.5001C12.4 16.4001 13.2 15.5001 14.2 15.5001C15.2 15.5001 16 16.4001 16 17.5001C16 18.6001 15.2 19.5001 14.2 19.5001ZM24.4 10.4001C23.1 9.8001 21.8 9.4001 20.3 9.1001H20.2C20 9.4001 19.8 9.8001 19.7 10.1001C18.2 9.9001 16.6 9.9001 15.1 10.1001C15 9.8001 14.8 9.4001 14.6 9.1001C14.6 9.1001 14.6 9.1001 14.5 9.1001C13.1 9.3001 11.7 9.8001 10.4 10.4001C7.8 14.3001 7.1 18.1001 7.4 21.8001C9.1 23.1001 10.8 23.8001 12.4 24.3001H12.5C12.9 23.8001 13.2 23.2001 13.5 22.6001V22.5001C13 22.3001 12.4 22.0001 11.9 21.8001V21.7001C12 21.6001 12.1 21.5001 12.2 21.5001C12.2 21.5001 12.2 21.5001 12.3 21.5001C15.6 23.0001 19.1 23.0001 22.4 21.5001C22.4 21.5001 22.4 21.5001 22.5 21.5001C22.6 21.6001 22.7 21.7001 22.8 21.7001V21.8001C22.3 22.1001 21.8 22.3001 21.2 22.5001C21.2 22.5001 21.1 22.6001 21.2 22.6001C21.5 23.2001 21.8 23.7001 22.2 24.3001C22.2 24.3001 22.2 24.3001 22.3 24.3001C23.9 23.8001 25.6 23.0001 27.3 21.8001C27.8 17.5001 26.7 13.8001 24.4 10.4001Z"
+        fill="white"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_32_180">
+        <rect width="35" height="35" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+const InstagramIcon = () => (
+  <svg
+    width="35"
+    height="35"
+    viewBox="0 0 35 35"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_32_183)">
+      <path
+        d="M17.5 35C7.9 35 0 27.1 0 17.5C0 7.9 7.9 0 17.5 0C27.1 0 35 7.9 35 17.5C35 27.1 27.1 35 17.5 35ZM17.5 1C8.4 1 1 8.4 1 17.5C1 26.6 8.4 34 17.5 34C26.6 34 34 26.6 34 17.5C34 8.4 26.6 1 17.5 1Z"
+        fill="white"
+      />
+      <path
+        d="M17.5 22.5C14.7 22.5 12.5 20.2 12.5 17.5C12.5 14.7 14.8 12.5 17.5 12.5C20.2 12.5 22.5 14.8 22.5 17.5C22.5 20.3 20.3 22.5 17.5 22.5ZM17.5 13.5C15.3 13.5 13.5 15.3 13.5 17.5C13.5 19.7 15.3 21.5 17.5 21.5C19.7 21.5 21.5 19.7 21.5 17.5C21.5 15.3 19.7 13.5 17.5 13.5Z"
+        fill="white"
+      />
+      <path
+        d="M23.9 12.3C23.9 12.8 23.5 13.1 23.1 13.1C22.6 13.1 22.3 12.7 22.3 12.3C22.3 11.8 22.7 11.5 23.1 11.5C23.5 11.5 23.9 11.9 23.9 12.3Z"
+        fill="white"
+      />
+      <path
+        d="M23.8 26.4001H11.2C9.70001 26.4001 8.60001 25.2001 8.60001 23.8001V11.2001C8.60001 9.7001 9.80001 8.6001 11.2 8.6001H23.7C25.2 8.6001 26.3 9.8001 26.3 11.2001V23.7001C26.4 25.2001 25.2 26.4001 23.8 26.4001ZM11.2 9.7001C10.3 9.7001 9.60001 10.4001 9.60001 11.3001V23.8001C9.60001 24.7001 10.3 25.4001 11.2 25.4001H23.7C24.6 25.4001 25.3 24.7001 25.3 23.8001V11.2001C25.3 10.3001 24.6 9.6001 23.7 9.6001H11.2V9.7001Z"
+        fill="white"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_32_183">
+        <rect width="35" height="35" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+const MirrorIcon = () => (
+  <svg
+    width="35"
+    height="35"
+    viewBox="0 0 35 35"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_32_175)">
+      <path
+        d="M17.5 35C7.9 35 0 27.1 0 17.5C0 7.9 7.9 0 17.5 0C27.1 0 35 7.9 35 17.5C35 27.1 27.1 35 17.5 35ZM17.5 1C8.4 1 1 8.4 1 17.5C1 26.6 8.4 34 17.5 34C26.6 34 34 26.6 34 17.5C34 8.4 26.6 1 17.5 1Z"
+        fill="white"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M9.29999 25.8C9.29999 26.6 9.89999 27.2 10.7 27.2H23.5C24.3 27.2 24.9 26.6 24.9 25.8V14.8C24.9 10.2 21.7 7 17.1 7C12.5 7 9.29999 10.2 9.29999 14.8V25.8ZM10.2 14.8C10.2 10.7 13 7.90002 17.1 7.90002C21.3 7.90002 24 10.7 24 14.8V25.8C24 26.1 23.8 26.3 23.5 26.3H10.7C10.4 26.3 10.2 26.1 10.2 25.8V14.8Z"
+        fill="white"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_32_175">
+        <rect width="35" height="35" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
 
 const data = [
   {
-    title: 'Community',
+    title: "Community",
     links: [
-      { label: 'HOLA@GIFME.BEER', link: '/' },
-      { label: 'FAQ', link: '/' },
-      { label: 'GIFME.BEER TEAM', link: '/' },
-      { label: 'TERMS & CONDITIONS', link: '/tos' },
+      { label: "ARE YOU IN THE BREWING BUSINESS? JOIN US!", link: "/" },
+      { label: "FAQ", link: "/" },
+      { label: "GIFME.BEER TEAM", link: "/" },
+      { label: "TERMS & CONDITIONS", link: "/tos" },
+      { label: "HOLA@GIFME.BEER", link: "/", color: "#ffff00" },
     ],
   },
 ];
@@ -22,10 +132,11 @@ export default function Footer() {
 
   const groups = data.map((group) => {
     const links = group.links.map((link, index) => (
-      <Text<'a'>
+      <Text<"a">
         key={index}
-        className={'link'}
-        component='a'
+        className={"link"}
+        c={link.color}
+        component="a"
         href={link.link}
         onClick={(event) => event.preventDefault()}
       >
@@ -35,10 +146,10 @@ export default function Footer() {
 
     return (
       <Container
-        w={{ base: '100vw', md: '100%' }}
-        maw='300px'
+        w={{ base: "100vw", sm: "100%" }}
+        maw="770px"
         fluid
-        dir='column'
+        dir="column"
         key={group.title}
       >
         {/* <Text className={'title'}>{group.title}</Text> */}
@@ -48,40 +159,26 @@ export default function Footer() {
   });
 
   return (
-    <footer className={'footer'}>
-      <Container className={'inner'}>
-        <div className={'groups'}>{groups}</div>
+    <footer className={"footer"}>
+      <Container className={"inner"}>
+        <div className={"groups"}>{groups}</div>
 
         <Group
-          gap={'lg'}
-          className={'social'}
-          justify='flex-end'
-          wrap='nowrap'
-          mt={{ base: 'xl', md: 0 }}
+          gap={"sm"}
+          className={"social"}
+          justify="flex-end"
+          wrap="nowrap"
+          mt={{ base: "xl", md: 0 }}
         >
-          <ActionIcon size='xl' color='white' variant='outline' radius='xl'>
-            <IconBrandXFilled
-              style={{ width: rem(32), height: rem(32) }}
-              stroke={1.5}
-            />
-          </ActionIcon>
-          <ActionIcon size='xl' color='white' variant='outline' radius='xl'>
-            <IconBrandDiscordFilled
-              style={{ width: rem(32), height: rem(32) }}
-              stroke={1.5}
-            />
-          </ActionIcon>
-          <ActionIcon size='xl' color='white' variant='outline' radius='xl'>
-            <IconBrandInstagram
-              style={{ width: rem(32), height: rem(32) }}
-              stroke={1.5}
-            />
-          </ActionIcon>
+          <TwitterIcon style={{ width: rem(35), height: rem(35) }} />
+          <DiscordIcon style={{ width: rem(35), height: rem(35) }} />
+          <InstagramIcon style={{ width: rem(35), height: rem(35) }} />
+          <MirrorIcon style={{ width: rem(35), height: rem(35) }} />
         </Group>
       </Container>
-      <Container className={'afterFooter'}>
-        <Text c='white' size='sm'>
-          © {currentYear} All rights reserved.
+      <Container className={"afterFooter"}>
+        <Text className={"afterFooter-t"} c="white" size="sm">
+          © {currentYear} of all contents Gifme.beer | 2024
         </Text>
       </Container>
     </footer>
