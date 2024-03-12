c14b554f60a6cc5a1c97515b920257d4df95a609
Mateo Daza <mateodaza@gmail.com>
2024-03-12 15:05:20 -0500

    moving to op mainnet

---
+++ src/constants/collections/festivals.ts
@@ -0,0 +1,21 @@
+import { ICollection } from '@/types';
+import { NFT_COLLECTION_ADDRESS } from '../addresses';
+
+export default {
+  id: 0,
+  name: 'Festivals',
+  address: NFT_COLLECTION_ADDRESS,
+  network: 'optimism',
+  chainId: 10,
+  color: 'rgba(99, 192, 172, 1)',
+  cards: [
+    {
+      name: 'BBF 2024',
+      address: NFT_COLLECTION_ADDRESS,
+      id: 0,
+      category: 'Festival',
+      imageUrl: '/images/collections/op-mainnet/bbf_2024.gif',
+      meta: {},
+    },
+  ],
+} as ICollection;
