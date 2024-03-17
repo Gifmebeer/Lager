4699dab3ebd5cbeb242a0658d9337cc6f0ae5928
lauracapella <laura@laguapacomunicacion.com>
2024-03-17 09:53:59 +0100

    breweries form validation

--- src/components/breweries/index.tsx
+++ src/components/breweries/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { useState } from 'react';
 import {
   Grid,
   Container,
@@ -10,24 +10,23 @@ import {
   Button,
   Group,
   Box,
-  Stack,
   Flex,
   Select,
 } from '@mantine/core';
 
 import { useMediaQuery } from '@mantine/hooks';
 
-import { useForm } from '@mantine/form';
+import { isInRange, isNotEmpty, useForm } from '@mantine/form';
 
 import Text from '../Text';
-import { fontSize } from '../../../node_modules/@thirdweb-dev/react/dist/declarations/src/design-system/index';
-import { loadGetInitialProps } from '../../../node_modules/next/dist/shared/lib/utils';
+
 
 const Breweries: React.FC = () => {
+   const [checked, setChecked] = useState(false);
   const isMobile = useMediaQuery(`(max-width: ${em(850)})`);
   const form = useForm({
     initialValues: {
-      brewerie: '',
+      brewerie: false,
       festival: false,
       taproom: false,
       shop: false,
@@ -45,13 +44,25 @@ const Breweries: React.FC = () => {
       ig: '',
       fb: '',
       beernum: '',
-      legal: '',
+      legal: {checked},
       newsletter: '',
     },
 
+    
     validate: {
+      company: (value) => (value.length < 2 ? 'Must have at least 2 letters' : null),
+      name: (value) => (value.length < 2 ? 'Must have at least 2 letters' : null),
+      city: (value) => (value.length < 2 ? 'Must have at least 2 letters' : null),
+      province: (value) => (value.length < 2 ? 'Must have at least 2 letters' : null),
+      country: isNotEmpty('Select country'),
       email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
-    },
+      phone: isNotEmpty('Fill in a phone'),
+      web: (value) => (/(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g.test(value) ? null : 'Invalid web adress'),
+      shoponline: isNotEmpty('Shop online?'),
+      tpowner: isNotEmpty('Tap owner?'),
+      legal: isNotEmpty('Terms of use must be accepted'),
+/*       legal: (value) => (!value ? 'Must have at least 2 letters' : null),
+ */    },
   });
 
   return (
@@ -86,18 +97,26 @@ const Breweries: React.FC = () => {
             direction="row"
             wrap="wrap"
           >
+ 
+      
+
             <Checkbox
-              
               mt="md"
-              m='lg'
-              label="BREWERY"
+              label="BREWERY"    
+                      
               styles={{
                 inner: {
                     order: 0,
                   },
+                  input: {
+                    border: '1px solid black',
+                  },
               }}
-              defaultChecked
-              {...form.getInputProps('brewery', { type: 'checkbox' })}
+              color="black" 
+                                
+              {...form.getInputProps('brewerie', { type: 'checkbox' })}
+              
+              
             />
 
             <Checkbox
@@ -107,7 +126,12 @@ const Breweries: React.FC = () => {
                 inner: {
                     order: 0,
                   },
+                  input: {
+                    border: '1px solid black',
+                  },
               }}
+              color="black"  
+              
               {...form.getInputProps('festival', { type: 'checkbox' })}
             />
             <Checkbox
@@ -117,7 +141,11 @@ const Breweries: React.FC = () => {
                 inner: {
                     order: 0,
                   },
+                  input: {
+                    border: '1px solid black',
+                  },
               }}
+              color="black" 
               {...form.getInputProps('taproom', { type: 'checkbox' })}
             />
             <Checkbox
@@ -127,7 +155,11 @@ const Breweries: React.FC = () => {
                 inner: {
                     order: 0,
                   },
+                  input: {
+                    border: '1px solid black',
+                  },
               }}
+              color="black" 
               {...form.getInputProps('shop', { type: 'checkbox' })}
             />
 
@@ -139,16 +171,25 @@ const Breweries: React.FC = () => {
                 inner: {
                     order: 0,
                   },
+                  input: {
+                    border: '1px solid black',
+                  },
               }}
+              color="black" 
               {...form.getInputProps('club', { type: 'checkbox' })}
             />
+
+
+ 
           </Flex>
 
           <Grid>
             <Grid.Col span={{ base: 12, md: 6 }}>
               <TextInput
-                label="COMPANY NAME / "
+                withAsterisk
+                label="COMPANY NAME "
                 placeholder="Company Name"
+                mb="lg"
                 styles={{
                   input: {
                     backgroundColor: 'white',
@@ -160,6 +201,7 @@ const Breweries: React.FC = () => {
             </Grid.Col>
             <Grid.Col span={{ base: 12, md: 6 }}>
               <TextInput
+                withAsterisk
                 label="CONTACT PERSON NAME"
                 placeholder="Name"
                 mb="lg"
@@ -174,7 +216,11 @@ const Breweries: React.FC = () => {
             </Grid.Col>
           </Grid>
 
+          
+          <Grid>
+            <Grid.Col span={{ base: 12, md: 4 }}>
           <TextInput
+            withAsterisk
             label="CITY / TOWN"
             placeholder="City - Town"
             mb="lg"
@@ -186,8 +232,11 @@ const Breweries: React.FC = () => {
             }}
             {...form.getInputProps('city')}
           />
+          </Grid.Col>
+            <Grid.Col span={{ base: 12, md: 4 }}>
 
           <TextInput
+            withAsterisk
             label="PROVINCE"
             placeholder="Province"
             mb="lg"
@@ -199,12 +248,242 @@ const Breweries: React.FC = () => {
             }}
             {...form.getInputProps('province')}
           />
-
+          </Grid.Col>
+          <Grid.Col span={{ base: 12, md: 4 }}>
           <Select
+            withAsterisk
             label="COUNTRY"
-            placeholder=""
-            data={['Spain', 'Portugal', 'France', '...']}
+            placeholder="Select country"
+            searchable
+            error
+            data={['Afghanistan',
+              'Albania',
+              'Algeria',
+              'Andorra',
+              'Angola',
+              'Antigua and Barbuda',
+              'Argentina',
+              'Armenia',
+              'Australia',
+              'Austria',
+              'Austrian Empire*',
+              'Azerbaijan',
+              'Baden*',
+'Bahamas, The',
+'Bahrain',
+'Bangladesh',
+'Barbados',
+'Bavaria*',
+'Belarus',
+'Belgium',
+'Belize',
+'Benin (Dahomey)',
+'Bolivia',
+'Bosnia and Herzegovina',
+'Botswana',
+'Brazil',
+'Brunei',
+'Brunswick and Lüneburg*',
+'Bulgaria',
+'Burkina Faso (Upper Volta)',
+'Burma',
+'Burundi',
+'Cabo Verde',
+'Cambodia',
+'Cameroon',
+'Canada',
+'Cayman Islands, The',
+'Central African Republic',
+'Central American Federation*',
+'Chad',
+'Chile',
+'China',
+'Colombia',
+'Comoros',
+'Congo Free State, The*',
+'Costa Rica',
+'Cote d Ivoire (Ivory Coast)',
+'Croatia',
+'Cuba',
+'Cyprus',
+'Czechia',
+'Czechoslovakia*',
+'Democratic Republic of the Congo',
+'Denmark',
+'Djibouti',
+'Dominica',
+'Dominican Republic',
+'Duchy of Parma, The*',
+'East Germany (German Democratic Republic)*',
+'Ecuador',
+'Egypt',
+'El Salvador',
+'Equatorial Guinea',
+'Eritrea',
+'Estonia',
+'Eswatini',
+'Ethiopia',
+'Federal Government of Germany (1848-49)*',
+'Fiji',
+'Finland',
+'France',
+'Gabon',
+'Gambia, The',
+'Georgia',
+'Germany',
+'Ghana',
+'Grand Duchy of Tuscany, The*',
+'Greece',
+'Grenada',
+'Guatemala',
+'Guinea',
+'Guinea-Bissau',
+'Guyana',
+'Haiti',
+'Hanover*',
+'Hanseatic Republics*',
+'Hawaii*',
+'Hesse*',
+'Holy See',
+'Honduras',
+'Hungary',
+'Iceland',
+'India',
+'Indonesia',
+'Iran',
+'Iraq',
+'Ireland',
+'Israel',
+'Italy',
+'Jamaica',
+'Japan',
+'Jordan',
+'Kazakhstan',
+'Kenya',
+'Kingdom of Serbia/Yugoslavia*',
+'Kiribati',
+'Korea',
+'Kosovo',
+'Kuwait',
+'Kyrgyzstan',
+'Laos',
+'Latvia',
+'Lebanon',
+'Lesotho',
+'Lew Chew (Loochoo)*',
+'Liberia',
+'Libya',
+'Liechtenstein',
+'Lithuania',
+'Luxembourg',
+'Madagascar',
+'Malawi',
+'Malaysia',
+'Maldives',
+'Mali',
+'Malta',
+'Marshall Islands',
+'Mauritania',
+'Mauritius',
+'Mecklenburg-Schwerin*',
+'Mecklenburg-Strelitz*',
+'Mexico',
+'Micronesia',
+'Moldova',
+'Monaco',
+'Mongolia',
+'Montenegro',
+'Morocco',
+'Mozambique',
+'Namibia',
+'Nassau*',
+'Nauru',
+'Nepal',
+'Netherlands, The',
+'New Zealand',
+'Nicaragua',
+'Niger',
+'Nigeria',
+'North German Confederation*',
+'North German Union*',
+'North Macedonia',
+'Norway',
+'Oldenburg*',
+'Oman',
+'Orange Free State*',
+'Pakistan',
+'Palau',
+'Panama',
+'Papal States*',
+'Papua New Guinea',
+'Paraguay',
+'Peru',
+'Philippines',
+'Piedmont-Sardinia*',
+'Poland',
+'Portugal',
+'Qatar',
+'Republic of Genoa*',
+'Republic of Korea (South Korea)',
+'Republic of the Congo',
+'Romania',
+'Russia',
+'Rwanda',
+'Saint Kitts and Nevis',
+'Saint Lucia',
+'Saint Vincent and the Grenadines',
+'Samoa',
+'San Marino',
+'Sao Tome and Principe',
+'Saudi Arabia',
+'Schaumburg-Lippe*',
+'Senegal',
+'Serbia',
+'Seychelles',
+'Sierra Leone',
+'Singapore',
+'Slovakia',
+'Slovenia',
+'Solomon Islands, The',
+'Somalia',
+'South Africa',
+'South Sudan',
+'Spain',
+'Sri Lanka',
+'Sudan',
+'Suriname',
+'Sweden',
+'Switzerland',
+'Syria',
+'Tajikistan',
+'Tanzania',
+'Texas*',
+'Thailand',
+'Timor-Leste',
+'Togo',
+'Tonga',
+'Trinidad and Tobago',
+'Tunisia',
+'Turkey',
+'Turkmenistan',
+'Tuvalu',
+'Two Sicilies*',
+'Uganda',
+'Ukraine',
+'Union of Soviet Socialist Republics*',
+'United Arab Emirates, The',
+'United Kingdom, The',
+'Uruguay',
+'Uzbekistan',
+'Vanuatu',
+'Venezuela',
+'Vietnam',
+'Württemberg*',
+'Yemen',
+'Zambia',
+'Zimbabwe' ]}
             defaultValue="Selecct"
+            mb="lg"
             clearable
             styles={{
               input: {
@@ -214,11 +493,20 @@ const Breweries: React.FC = () => {
               option: {
                 fontFamily: 'GT-America',
               },
+              
             }}
+            {...form.getInputProps("country")}
           />
+          </Grid.Col>
+          </Grid>
 
-          <TextInput
-            mt="md"
+        
+
+
+          <Grid>
+            <Grid.Col span={{ base: 12, md: 6 }}>
+            
+            <TextInput
             withAsterisk
             label="E-MAIL"
             mb="lg"
@@ -231,34 +519,14 @@ const Breweries: React.FC = () => {
             placeholder="your@email.com"
             {...form.getInputProps('email')}
           />
-
-          
-
-
-          
-
-          <Grid>
-            <Grid.Col span={{ base: 12, md: 6 }}>
-            <NumberInput
-            label="PHONE NUMBER"
-            prefix="+"
-            placeholder="Phone"
-            mb="lg"
-            styles={{
-              input: {
-                backgroundColor: 'white',
-                border: '1px solid black',
-              },
-            }}
-            {...form.getInputProps('phone')}
-          />
-              
+            
             </Grid.Col>
             <Grid.Col span={{ base: 12, md: 6 }}>
             <NumberInput
             label="PHONE NUMBER"
             placeholder="Phone"
             mb="lg"
+            format="+7 (000)000 0000" 
             styles={{
               input: {
                 backgroundColor: 'white',
@@ -284,13 +552,15 @@ const Breweries: React.FC = () => {
           />
 
           <Grid>
-            <Grid.Col span={{ base: 12, md: 6 }}>
+            <Grid.Col span={{ base: 12, md: 4 }}>
               <Select
+              
                 label="SHOP ONLINE"
-                placeholder=""
+                placeholder="Shop online"
                 data={['Si', 'No']}
                 defaultValue="Selecct"
                 clearable
+                mb="lg"
                 styles={{
                   input: {
                     backgroundColor: 'white',
@@ -303,13 +573,14 @@ const Breweries: React.FC = () => {
                 {...form.getInputProps('shoponline')}
               />
             </Grid.Col>
-            <Grid.Col span={{ base: 12, md: 6 }}>
+            <Grid.Col span={{ base: 12, md: 4 }}>
               <Select
                 label="DO YOU OWN A TAPROOM?"
-                placeholder=""
+                placeholder="Do you own a taproom?"
                 data={['Si', 'No']}
                 defaultValue="Selecct"
                 clearable
+                mb="lg"
                 styles={{
                   input: {
                     backgroundColor: 'white',
@@ -322,6 +593,20 @@ const Breweries: React.FC = () => {
                 {...form.getInputProps('tpowner')}
               />
             </Grid.Col>
+            <Grid.Col span={{ base: 12, md: 4 }}>
+            <NumberInput
+                label="HOW MANY BEERS DO YOU PRODUCE? "
+                placeholder="15200"
+                mb="lg"
+                styles={{
+                  input: {
+                    backgroundColor: 'white',
+                    border: '1px solid black',
+                  },
+                }}
+                {...form.getInputProps('beernum')}
+              />
+            </Grid.Col>
           </Grid>
 
           <Grid>
@@ -329,6 +614,7 @@ const Breweries: React.FC = () => {
               <TextInput
                 label="INSTAGRAM "
                 placeholder=""
+                mb="lg"
                 styles={{
                   input: {
                     backgroundColor: 'white',
@@ -354,53 +640,46 @@ const Breweries: React.FC = () => {
             </Grid.Col>
           </Grid>
 
-          <Grid>
-            <Grid.Col span={{ base: 12, md: 6 }}>
-              <NumberInput
-                label="HOW MANY BEERS DO YOU PRODUCE? "
-                placeholder=""
-                styles={{
-                  input: {
-                    backgroundColor: 'white',
-                    border: '1px solid black',
-                  },
-                }}
-                {...form.getInputProps('beernum')}
-              />
-            </Grid.Col>
-            <Grid.Col span={{ base: 12, md: 6 }}></Grid.Col>
-          </Grid>
+         
 
           <Checkbox
               mt="md"
-              m='lg'
+              mb="md"
               label="I have read and agree to the website Legal Terms*"
               styles={{
                 inner: {
                     order: 0,
                   },
+                  input: {
+                    border: '1px solid black',
+                  },
               }}
-              defaultChecked
-              {...form.getInputProps('brewery', { type: 'legal' })}
+              color="black" 
+              checked={checked}
+              onChange={(event) => setChecked(event.currentTarget.checked)}  
+              {...form.getInputProps('legal', { type: 'checkbox' })}
+              
             />
 
             <Checkbox
               mt="md"
-              m='lg'
               label="I want to receive news from gefme.beer"
-              
               styles={{
                 inner: {
                     order: 0,
                   },
+                  input: {
+                    border: '1px solid black',
+                  },
               }}
+              color="black" 
               defaultChecked
-              {...form.getInputProps('brewery', { type: 'newsletter' })}
+              {...form.getInputProps('newsletter', { type: 'checkbox' })}
             />
 
 
           <Group justify="flex-end" mt="md">
-          <Button type="submit"  ff='MetamorBit-Latin' variant="filled" color="rgba(0, 0, 0, 1)" size='xl'>SEND</Button>
+          <Button type="submit"  ff='MetamorBit-Latin' variant="filled" color="rgba(0, 0, 0, 1)" size="md" >SEND</Button>
         </Group>
 
         
