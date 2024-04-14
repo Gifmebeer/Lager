f452f1f106279b45775bd9c0bfe0eca4ac22a983
Mateo Daza <mateodaza@gmail.com>
2024-04-14 09:29:47 -0500

    adds new beers

---
+++ src/constants/collections/prelaunch/maresme.ts
@@ -0,0 +1,13 @@
+import { NFT_COLLECTION_ADDRESS } from '../../addresses';
+
+export default [
+  {
+    name: 'Garbia',
+    address: NFT_COLLECTION_ADDRESS,
+    id: 10,
+    category: 'Beer',
+    imageUrl:
+      '/images/collections/op-mainnet/maresme/MARESME_BREWERY_Garbia.png',
+    meta: {},
+  },
+];
