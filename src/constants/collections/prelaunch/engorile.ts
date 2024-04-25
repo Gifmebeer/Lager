4e33bbc0ce768d46411d366190daa82c230fe9b9
Mateo Daza <mateodaza@gmail.com>
2024-04-24 22:16:48 -0500

    uploading more nfts

---
+++ src/constants/collections/prelaunch/engorile.ts
@@ -0,0 +1,13 @@
+import { NFT_COLLECTION_ADDRESS } from '../../addresses';
+
+export default [
+  {
+    name: 'Engorile V Anniversary',
+    address: NFT_COLLECTION_ADDRESS,
+    id: 12,
+    category: 'Beer',
+    imageUrl:
+      '/images/collections/op-mainnet/engorile/Engorile_V-Aniversari-Edition.png',
+    meta: {},
+  },
+];
